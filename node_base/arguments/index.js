221d03f8d23186a74ed33c981e1c84505966fd8b
Esaan Alencar <esaanalencar250@gmail.com>
2024-07-29 21:22:48 -0300

    Delete node_base/arguments directory

--- node_base/arguments/index.js
+++
@@ -1,6 +0,0 @@
-const soma = require("./soma.js");
-const minimist = require("minimist");
-const args = minimist(process.argv.slice(2));
-const a = parseInt(args["a"]);
-const b = parseInt(args["b"]);
-soma(a, b);
