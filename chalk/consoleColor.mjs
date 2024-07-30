e9be14761701318302210f101d425dadc232b7b1
Esaan Alencar <esaanalencar250@gmail.com>
2024-07-29 21:15:45 -0300

    Delete chalk directory

--- chalk/consoleColor.mjs
+++
@@ -1,8 +0,0 @@
-import chalk from "chalk";
-const args = process.argv.slice(2);
-const argument = args[0];
-if (argument >= 5) {
-  console.log(chalk.green("Você foi aprovado!"));
-} else {
-  console.log(chalk.red("Vixe! Tá reprovado!"));
-}
