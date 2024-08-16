68c0fc75718f224995a521e81e374899a4009117
Esaan Alencar <esaanalencar250@gmail.com>
2024-08-16 19:22:46 -0300

    Create removeAmount.mjs

---
+++ projects/accounts/withdraw/removeAmount.mjs
@@ -0,0 +1,29 @@
+import fs from "fs";
+import chalk from "chalk";
+import getAccount from "../getAccount/getAccount.mjs";
+import withdraw from "./withdraw.mjs";
+import operation from "../index.mjs";
+
+const removeAmount = (accountName, amount) => {
+  const account = getAccount(accountName);
+  if (!amount) {
+    console.log(
+      chalk.bgRed.black("Ocorreu um erro! Tente novamente mais tarde...")
+    );
+    return withdraw();
+  }
+  if (account.balance < amount) {
+    console.log(chalk.bgRed.black("Valor indisponível!"));
+  }
+  account.balance = parseFloat(account.balance) - parseFloat(amount);
+  fs.writeFileSync(
+    `accounts/${accountName}.json`,
+    JSON.stringify(account),
+    (error) => {
+      console.log(error);
+    }
+  );
+  console.log(chalk.green(`Saque de R$ ${amount} realizado com sucesso.`));
+  operation();
+};
+export default removeAmount;
