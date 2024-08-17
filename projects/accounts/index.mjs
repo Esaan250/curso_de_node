811ee73e54604c7dc8b7a7be8e5028e14fbf244d
Esaan Alencar <esaanalencar250@gmail.com>
2024-08-16 21:37:59 -0300

    Update index.mjs

--- projects/accounts/index.mjs
+++ projects/accounts/index.mjs
@@ -1,43 +1,30 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
-import createAccount from "./createAccount/createAccount.mjs";
-import deposit from "./deposit/deposit.mjs";
-import getAccountBalance from "./getAccountBalance/getAccountBalance.mjs";
-import withdraw from "./withdraw/withdraw.mjs";
-const operation = () => {
+import createAccount from "./account/createAccount.mjs";
+import login from "./account/login.mjs";
+const entryAccount = () => {
+  console.log(chalk.green("Seja bem-vindo ao Accounts!"));
   inquirer
     .prompt([
       {
         type: "list",
         name: "action",
         message: "O que deseja fazer?",
-        choices: [
-          "Criar Conta",
-          "Consultar Saldo",
-          "Depositar",
-          "Sacar",
-          "Sair",
-        ],
+        choices: ["Fazer Login", "Criar Conta", "Sair"],
       },
     ])
     .then((answer) => {
       const action = answer["action"];
-      if (action === "Criar Conta") {
+      if (action === "Fazer Login") {
+        login();
+      } else if (action === "Criar Conta") {
         createAccount();
-      } else if (action === "Consultar Saldo") {
-        getAccountBalance();
-      } else if (action === "Depositar") {
-        deposit();
-      } else if (action === "Sacar") {
-        withdraw();
       } else if (action === "Sair") {
         console.log(chalk.bgBlue.black("Obrigado por usar o Accounts!"));
         process.exit();
       }
-    })
-    .catch((error) => {
-      console.log(error);
     });
 };
-operation();
-export default operation;
+entryAccount();
+
+export default entryAccount;
