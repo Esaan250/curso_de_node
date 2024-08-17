4c5f7b4cf4cb370d8869b82a97052b30a6ae3f07
Esaan Alencar <esaanalencar250@gmail.com>
2024-08-16 21:39:25 -0300

    Delete projects/accounts/createAccount directory

--- projects/accounts/createAccount/createAccount.mjs
+++
@@ -1,8 +0,0 @@
-import chalk from "chalk";
-import buildAccount from "./buildAccount.mjs";
-const createAccount = () => {
-  console.log(chalk.bgGreen.black("Obrigado por escolher o nosso banco!"));
-  console.log(chalk.green("Defina as opções da sua conta a seguir:"));
-  buildAccount();
-};
-export default createAccount;
