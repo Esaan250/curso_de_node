bb33e70f9520802cc731eb5daa271bae77db8aea
Esaan Alencar <esaanalencar250@gmail.com>
2024-08-16 21:36:54 -0300

    Update withdraw.mjs

--- projects/accounts/withdraw/withdraw.mjs
+++ projects/accounts/withdraw/withdraw.mjs
@@ -1,34 +1,17 @@
 import inquirer from "inquirer";
 import removeAmount from "./removeAmount.mjs";
-import checkAccount from "../checkAccount/checkAccount.mjs";
 
-const withdraw = () => {
+const withdraw = (accountName) => {
   inquirer
     .prompt([
       {
-        name: "accountName",
-        message: "Qual o nome da sua conta?",
+        name: "amount",
+        message: "Quanto quer sacar?",
       },
     ])
     .then((answer) => {
-      const accountName = answer["accountName"];
-      if (!checkAccount(accountName)) {
-        return withdraw();
-      }
-      inquirer
-        .prompt([
-          {
-            name: "amount",
-            message: "Quanto quer sacar?",
-          },
-        ])
-        .then((answer) => {
-          const amount = answer["amount"];
-          removeAmount(accountName, amount);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      const amount = answer["amount"];
+      removeAmount(accountName, amount);
     })
     .catch((error) => {
       console.log(error);
