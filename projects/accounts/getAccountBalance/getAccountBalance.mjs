3010eab3621a04cb234b50932d552c2b19239b0e
Esaan Alencar <esaanalencar250@gmail.com>
2024-08-16 21:36:31 -0300

    Update getAccountBalance.mjs

--- projects/accounts/getAccountBalance/getAccountBalance.mjs
+++ projects/accounts/getAccountBalance/getAccountBalance.mjs
@@ -1,28 +1,9 @@
 import chalk from "chalk";
-import inquirer from "inquirer";
 import getAccount from "../getAccount/getAccount.mjs";
-import checkAccount from "../checkAccount/checkAccount.mjs";
-import operation from "../index.mjs";
-
-const getAccountBalance = () => {
-  inquirer
-    .prompt([
-      {
-        name: "accountName",
-        message: "Qual o nome da sua conta?",
-      },
-    ])
-    .then((answer) => {
-      const accountName = answer["accountName"];
-      if (!checkAccount(accountName)) {
-        return getAccountBalance();
-      }
-      const account = getAccount(accountName);
-      console.log(chalk.bgBlue.black(`Seu saldo é de R$ ${account.balance}.`));
-      operation();
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+import operation from "../operation/operation.mjs";
+const getAccountBalance = (accountName) => {
+  const account = getAccount(accountName);
+  console.log(chalk.bgBlue.black(`Seu saldo é de R$ ${account.balance}.`));
+  operation(accountName);
 };
 export default getAccountBalance;
