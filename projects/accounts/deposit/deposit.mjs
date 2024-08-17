f5fc4591ec530d52dc22242dc6fd07a546c14370
Esaan Alencar <esaanalencar250@gmail.com>
2024-08-16 21:35:23 -0300

    Update deposit.mjs

--- projects/accounts/deposit/deposit.mjs
+++ projects/accounts/deposit/deposit.mjs
@@ -1,35 +1,18 @@
 import inquirer from "inquirer";
-import checkAccount from "../checkAccount/checkAccount.mjs";
 import addAmount from "./addAmount.mjs";
-import operation from "../index.mjs";
-const deposit = () => {
+import operation from "../operation/operation.mjs";
+const deposit = (accountName) => {
   inquirer
     .prompt([
       {
-        name: "accountName",
-        message: "Qual o nome da sua conta?",
+        name: "amount",
+        message: "Quanto deseja depositar?",
       },
     ])
     .then((answer) => {
-      const accountName = answer["accountName"];
-      if (!checkAccount(accountName)) {
-        return deposit();
-      }
-      inquirer
-        .prompt([
-          {
-            name: "amount",
-            message: "Quanto deseja depositar?",
-          },
-        ])
-        .then((answer) => {
-          const amount = answer["amount"];
-          addAmount(accountName, amount);
-          operation();
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      const amount = answer["amount"];
+      addAmount(accountName, amount);
+      operation(accountName);
     })
     .catch((error) => {
       console.log(error);
