bcb9531f91149dbb9e0943af0d297698189dd4c0
Esaan Alencar <esaanalencar250@gmail.com>
2024-10-08 21:14:05 -0300

    Create Task.mjs

---
+++ mvc/models/Task.mjs
@@ -0,0 +1,17 @@
+import { DataTypes } from "sequelize";
+import db from "../db/connection.mjs";
+const Task = db.define("Task", {
+  title: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+  description: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+  done: {
+    type: DataTypes.BOOLEAN,
+    required: true,
+  },
+});
+export default Task;
