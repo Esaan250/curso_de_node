96ff9ddfc26490fc38ff801997da165c951b3cb5
Esaan Alencar <esaanalencar250@gmail.com>
2024-10-09 21:55:44 -0300

    Update tasksRoutes.mjs

--- mvc/routes/tasksRoutes.mjs
+++ mvc/routes/tasksRoutes.mjs
@@ -1,6 +1,9 @@
 import express from "express";
 import TaskController from "../controllers/TaskController.mjs";
 const router = express.Router();
-router.get("/add", TaskController.createTask);
 router.get("/", TaskController.showTasks);
+router.get("/add", TaskController.createTask);
+router.post("/add", TaskController.createTaskPost);
+router.post("/remove", TaskController.removeTasks);
+router.get("/edit/:id", TaskController.updateTask);
 export default router;
