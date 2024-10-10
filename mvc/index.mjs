89a075d24b3f320bedab1127220c74362c75ebbd
Esaan Alencar <esaanalencar250@gmail.com>
2024-10-09 21:59:45 -0300

    Update index.mjs

--- mvc/index.mjs
+++ mvc/index.mjs
@@ -1,13 +1,19 @@
 import express from "express";
+import { Router } from "express";
 import exphbs from "express-handlebars";
 import Task from "./models/Task.mjs";
 import connection from "./db/connection.mjs";
+import tasksRoutes from "./routes/tasksRoutes.mjs";
 const app = express();
-app.engine(".hbs", exphbs.engine({ extname: ".hbs", defaultLayout: "main" }));
+const hbs = exphbs.create({
+  partialsDir: ["views/partials"],
+});
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use("/tasks", tasksRoutes);
 connection
   .sync()
   .then(() => {
