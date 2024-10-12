f008b6f60f83047b97d770422f371b3efd1d06bd
Esaan Alencar <esaanalencar250@gmail.com>
2024-10-12 12:58:37 -0300

    Add files via upload

---
+++ sequelize/models/Adress.mjs
@@ -0,0 +1,20 @@
+import { DataTypes, STRING } from "sequelize";
+import db from "../db/connection.mjs";
+import User from "./User.mjs";
+const Address = db.define("Address", {
+  street: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+  number: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+  city: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+});
+User.hasMany(Address);
+Address.belongsTo(User);
+export default Address;
