c9062dfda94fcc2ce81d201b0a66219d111e467b
Esaan Alencar <esaanalencar250@gmail.com>
2024-10-21 21:20:37 -0300

    Update ToughtsController.mjs

--- toughts/controllers/ToughtsController.mjs
+++ toughts/controllers/ToughtsController.mjs
@@ -1,7 +1,82 @@
 import Tought from "../models/Tought.mjs";
 import User from "../models/User.mjs";
-export default class ToughtsControllers {
-  static async showToughts(req, res) {
-    res.render("toughts/home");
+import { Op } from "sequelize";
+export default class ToughController {
+  static async dashboard(req, res) {
+    const userId = req.session.userid;
+    const user = await User.findOne({
+      where: {
+        id: userId,
+      },
+      include: Tought,
+      plain: true,
+    });
+    const toughts = user.Toughts.map((result) => result.dataValues);
+    let emptyToughts = true;
+    if (toughts.length > 0) {
+      emptyToughts = false;
+    }
+    console.log(toughts);
+    console.log(emptyToughts);
+    res.render("toughts/dashboard", { toughts, emptyToughts });
+  }
+  static createTought(req, res) {
+    res.render("toughts/create");
+  }
+  static createToughtSave(req, res) {
+    const tought = {
+      title: req.body.title,
+      UserId: req.session.userid,
+    };
+    Tought.create(tought)
+      .then(() => {
+        req.flash("message", "Pensamento criado com sucesso!");
+        req.session.save(() => {
+          res.redirect("/toughts/dashboard");
+        });
+      })
+      .catch((err) => console.log());
+  }
+  static showToughts(req, res) {
+    console.log(req.query);
+    // check if user is searching
+    let search = "";
+    if (req.query.search) {
+      search = req.query.search;
+    }
+    // order results, newest first
+    let order = "DESC";
+    if (req.query.order === "old") {
+      order = "ASC";
+    } else {
+      order = "DESC";
+    }
+    Tought.findAll({
+      include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` },
+      },
+      order: [["createdAt", order]],
+    })
+      .then((data) => {
+        let toughtsQty = data.length;
+        if (toughtsQty === 0) {
+          toughtsQty = false;
+        }
+        const toughts = data.map((result) => result.get({ plain: true }));
+        res.render("toughts/home", { toughts, toughtsQty, search });
+      })
+      .catch((err) => console.log(err));
+  }
+  static removeTought(req, res) {
+    const id = req.body.id;
+    Tought.destroy({ where: { id: id } })
+      .then(() => {
+        req.flash("message", "Pensamento removido com sucesso!");
+        req.session.save(() => {
+          res.redirect("/toughts/dashboard");
+        });
+      })
+      .catch((err) => console.log());
   }
 }
