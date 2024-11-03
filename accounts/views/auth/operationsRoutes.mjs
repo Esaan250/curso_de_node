4f900cb31449a372b17de7c4142318c8757e6d16
Esaan Alencar <esaanalencar250@gmail.com>
2024-11-03 16:44:44 -0300

    Delete accounts/views/auth/operationsRoutes.mjs

--- accounts/views/auth/operationsRoutes.mjs
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import checkAuth from "../helpers/auth.mjs";
-import OperationsController from "../controllers/bankOperationsController.mjs";
-const router = Router();
-router.get("/home", checkAuth, OperationsController.operationsPage);
-export default router;
