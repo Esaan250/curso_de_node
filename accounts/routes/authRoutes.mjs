20a70c4f6fd25f351fcd52cba4f24360afe384e6
Esaan Alencar <esaanalencar250@gmail.com>
2024-11-03 16:43:11 -0300

    Update authRoutes.mjs

--- accounts/routes/authRoutes.mjs
+++ accounts/routes/authRoutes.mjs
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import AuthController from "../controllers/AuthController.mjs";
+import AuthController from "../controllers/authController.mjs";
 const router = Router();
-router.get("/login", AuthController.login);
+router.get("/login", AuthController.loginPage);
+router.post("/login", AuthController.loginPost);
+router.get("/register", AuthController.registerPage);
+router.post("/register", AuthController.registerPost);
+router.get("/logout", AuthController.logout);
 export default router;
